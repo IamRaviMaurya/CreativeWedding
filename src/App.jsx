577d83bf0ffb3dc257f3f66a75956a4f9815c467
Ravi Maurya <ravi.m@efycons.ai>
2026-05-18 17:10:44 +0530

    Feat: added changes

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ export default function App() {
       <Header />
       <main>
         <Hero />
-        <ProofStrip />
+        {/* <ProofStrip /> */}
         <DecorativeFrame />
         <About />
         <DecorativeFrame />
